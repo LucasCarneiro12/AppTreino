e50ba75ba164745d0538401a400a79dd48f208ce
levii <levii_gomes@hotmail.com>
2024-11-08 14:53:53 -0300

    aba masculino a

--- app/src/main/java/com/example/primeiroaplicativo/TreinoA.java
+++ app/src/main/java/com/example/primeiroaplicativo/TreinoA.java
@@ -19,9 +19,36 @@ public class TreinoA extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_treino_a);
 
+        //crucifixo
         ImageView crucifixo = findViewById(R.id.gif_crucifixo);
         Glide.with(this).asGif().load(R.drawable.crucifixo_reto).into(crucifixo);
+        //supino inclinado
+        ImageView supinoinclinado = findViewById(R.id.gif_supinoinclinado);
+        Glide.with(this).asGif().load(R.drawable.supinoinclinado).into(supinoinclinado);
+        //supino maquina
+        ImageView supinomaquina = findViewById(R.id.gif_supinomaquina);
+        Glide.with(this).asGif().load(R.drawable.supinomaquina).into(supinomaquina);
+        //supino reto
+        ImageView supino_reto = findViewById(R.id.gif_supinoreto);
+        Glide.with(this).asGif().load(R.drawable.supino_reto).into(supino_reto);
 
+
+        //elevacao lateral
+        ImageView elevacaolateral = findViewById(R.id.gif_elevacaolateral);
+        Glide.with(this).asGif().load(R.drawable.elevacaolateral).into(elevacaolateral);
+
+        //desenvolvimento maquina
+        ImageView desenvolvimentomaquina = findViewById(R.id.gif_desenvolvimentomaquina);
+        Glide.with(this).asGif().load(R.drawable.desenvolvimentomaquina).into(desenvolvimentomaquina);
+
+        //tricepspuley
+        ImageView tricepspuley = findViewById(R.id.gif_tricepspuley);
+        Glide.with(this).asGif().load(R.drawable.tricepspuley).into(tricepspuley);
+
+
+        //tricepsdip
+        ImageView tricepsdip = findViewById(R.id.gif_tricepsdip);
+        Glide.with(this).asGif().load(R.drawable.tricepsdip).into(tricepsdip);
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
