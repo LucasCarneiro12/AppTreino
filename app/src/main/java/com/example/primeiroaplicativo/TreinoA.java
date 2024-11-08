427a1dc5e56fbb9e186ad7a87406ee1906323dcb
levii <levii_gomes@hotmail.com>
2024-11-08 14:11:20 -0300

    a

--- app/src/main/java/com/example/primeiroaplicativo/TreinoA.java
+++ app/src/main/java/com/example/primeiroaplicativo/TreinoA.java
@@ -23,6 +23,7 @@ public class TreinoA extends AppCompatActivity {
         Glide.with(this).asGif().load(R.drawable.crucifixo_reto).into(crucifixo);
 
 
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
